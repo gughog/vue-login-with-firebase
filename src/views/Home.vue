3eae1613089066c7d1abce52bc7c265525b8450f
gughog <gustavogiovanne3@gmail.com>
2019-10-11 21:44:57 -0300

    Adicionado a grande recompensa ao fazer login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    THIS IS HOME!
+    <h1 class="x-center">Yey, você achou o gatineo fofineo!</h1>
+    <div class="x-center">
+      <img src="https://media.giphy.com/media/5UVFr72cJmnoQ/giphy.gif" alt="Cute Kitty">
+    </div>
   </div>
 </template>
 
